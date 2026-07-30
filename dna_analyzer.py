533bdb55d4f96f5c561fa8bcdb946acaf09a5965
parmaraxita <parmaraxita8@gmail.com>
2026-07-30 22:51:26 +0530

    Create dna_analyzer.py

---
+++ dna_analyzer.py
@@ -0,0 +1,16 @@
+dna = input('Enter a DNA sequence: ')
+length = len(dna)
+a = dna.count('A')
+t = dna.count('T')
+g = dna.count('G')
+c = dna.count('C')
+gc_content = ( (g+c) / length) * 100
+print('\n-----DNA Analysis -----')
+print('Sequence:', dna)
+print('Length:',length)
+print('A:',a)
+print('T:',t)
+print('G:',g)
+print('C:',c)
+print('GC Content: {: .2f}
+      %'.format(gc_conent))
